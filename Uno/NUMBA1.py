0bfaea7d3f6227fdace56c899ca95d74907a6da1
NovaZ900 <ediaebosede@gmail.com>
2025-07-17 18:17:36 +0100

    kaakak

--- Uno/NUMBA1.py
+++ Uno/NUMBA1.py
@@ -1,3 +1,24 @@
-print("jsjasj")
-print("jssjskjsk")
-print("ksokdsmdskm")
+import pgzrun
+import turtle
+
+WIDTH = 1400
+HEIGHT = 700
+def l():
+    g.left(90)
+    bg.update()
+def r():
+    g.right(90)
+    bg.update()
+def f():
+    g.forward(10)
+    bg.update()
+bg = turtle.Screen()
+bg.tracer(0)
+bg.bgcolor("dark blue")
+bg.listen()
+g = turtle.Turtle()
+g.shape("circle")
+g.color("white")
+bg.update()
+
+pgzrun.go()
