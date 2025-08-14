9555333a34ca204facaf230fcbaabf5063361078
NovaZ900 <ediaebosede@gmail.com>
2025-08-14 19:38:44 +0100

    kaka

--- Uno/NUMBA3.5.py
+++ Uno/NUMBA3.5.py
@@ -7,6 +7,9 @@ HEIGHT = 700
 score = 0
 
 ss = []
+sc = []
+ec = []
+sn = 1
 for i in range(8):
     s = Actor("satlite")
     s.pos = random.randint(10,650),random.randint(10,650)
@@ -18,7 +21,13 @@ def draw():
     screen.blit("space",(0,0))
     for i in range(8):
         ss[i].draw()
-    screen.draw.text(f"{score}/8",center = (100,100),fontsize = (20),color = ("white"))
+        screen.draw.text(f"{i+1}",center = (ss[i].x,ss[i].y-10),color = ("white"))
+    
+    for i in range(len(sc)):
+        screen.draw.line(sc[i],ec[i],color = "white")
+    screen.draw.text(f"{score}/7",center = (100,100),fontsize = (20),color = ("white"))
+
+
 
 def update():
     pass
@@ -26,10 +35,25 @@ def update():
 def on_mouse_down(pos):
     global score
     global cord
+    global sn
+    global sc
+    global ec
     cord = pos
-    if s.collidepoint(pos):
-        if score < 8:
-            score += 1
+    for i in range(8):
+        if ss[i].collidepoint(pos):
+            if i == sn:
+                sc.append(ss[i-1].pos)
+                ec.append(ss[i].pos)
+                if score < 7:
+                    score += 1
+                sn += 1
+            else:
+                sc = []
+                ec = []
+                sn = 1
+        if score == 7:
+            exit()
+
 
 
 
