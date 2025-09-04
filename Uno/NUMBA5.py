deae8785f1d72c3b1221d1160da1917503be45a6
NovaZ900 <ediaebosede@gmail.com>
2025-09-04 19:11:40 +0100

    jajajajajajajajlksdfkhkfdshbfeskmfi

---
+++ Uno/NUMBA5.py
@@ -0,0 +1,51 @@
+import pgzrun
+import random
+
+WIDTH = 700
+HEIGHT = 700
+
+jaja = ["ball","drop","satlite","square"]
+lvl = 1
+sjaja = []
+l = Actor("leaf")
+an = []
+
+def setup():
+    for i in range(lvl):
+        s = Actor(random.choice(jaja))
+        sjaja.append(s)
+    sjaja.append(l)
+    random.shuffle(sjaja)
+    g = WIDTH / (lvl + 2)
+    for i in range(lvl + 1):
+        sjaja[i].pos = g * (i + 1) , 20
+
+def anit():
+    for i in sjaja:
+        a = animate(i , duration = 5, y = 750)
+        an.append(a)
+
+setup()
+anit()
+
+def draw():
+    screen.blit("void", (0,0))
+    for i in sjaja:
+        i.draw()
+
+def on_mouse_down(pos):
+    global lvl, an, sjaja
+    for i in sjaja:
+        if i.collidepoint(pos):
+            if i == l:
+                lvl += 1
+                for j in an:
+                    j.stop()
+                an = []
+                sjaja = []
+                setup()
+                anit()
+                break
+                
+
+pgzrun.go()
---
+++ Uno/gFo.py
@@ -0,0 +1,65 @@
+import pgzrun
+
+WIDTH = 700
+HEIGHT = 700
+
+s = Actor("ball")
+b = Actor("bullet")
+
+s.pos = 350 , 680
+
+lvl = 1
+sl = []
+anjj = []
+
+k = False
+
+b.pos = s.pos
+
+def setup():
+    for i in range(lvl):
+        sll = Actor("red_ball")
+        sl.append(sll)
+    g = WIDTH / (lvl + 2)
+    for i in range(lvl):
+        sl[i].pos = g * (i + 1) , 20
+
+def anit():
+    for i in range(len(sl)):
+        print(i)
+        if i // 2 == 0:
+            a = animate(sl[i] , duration = 5, x = 500)
+        elif i // 2 != 0:
+            a = animate(sl[i] , duration = 5, x = 250)
+        anjj.append(a)
+        sl[i].y += 3
+
+setup()
+anit()
+
+def draw():
+    screen.blit("void", (0,0))
+    for i in sl:
+        i.draw()
+    if k == True:
+        b.draw()
+
+def update():
+    global k
+    global lvl
+    b.y -= 3
+    if keyboard.a:
+        s.x -= 3
+    if keyboard.d:
+        s.x += 3
+    if keyboard.space:
+        k = True
+    for i in range(lvl):
+        if b.colliderect(sl[i]):
+            lvl += 1
+        if s.colliderect(sl[i]):
+            exit()
+            
+    
+
+pgzrun.go()
