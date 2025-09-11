b561fcdb1cd46e4f651fc8617b74b56cb227aafa
NovaZ900 <ediaebosede@gmail.com>
2025-09-11 20:00:03 +0100

    djdhjgfrhjgrgfmfgrtgtfrgffkgrkjfklfkmfdkfdkdfkdfdfkjldfdfkjlkjldfkjldfkjlkjldfkjldfkjlfkjlkjlkjkjlfkjlfkj

--- Uno/NUMBA6.py
+++ Uno/NUMBA6.py
@@ -1,4 +1,7 @@
 import pgzrun
+import pygame
+
+m = pygame.mixer.Sound("Uno\eep.wav")
 
 WIDTH = 700
 HEIGHT = 700
@@ -30,22 +33,24 @@ setup()
 def draw():
     screen.blit("void", (0,0))
     s.draw()
-    for i in range(lvl):
-        setupp[i].draw()
+    for r in setupp:
+        r.draw()
     for b in bully:
         b.draw()
 
 dx = 3
+dy = 20
 
 def update():
-    global dx , j , lvl
+    global dx , j , lvl, dy, setupp, bully
     jb = 0
     leng = lvl - jb
-    for i in range(lvl):
-        setupp[i].x += dx
-        if setupp[i].x > 700 or setupp[i].x < 0:
-            dx*=-1
-            setupp[i].y += 30
+    for r in setupp:
+        r.x += dx
+        r.y = dy
+    if setupp[-1].x > 700 or setupp[0].x < 0:
+        dx*=-1
+        dy += 30
     for b in bully:
         b.y -= 3
         if b.y < 0: 
@@ -54,24 +59,26 @@ def update():
         s.x -= 3
     if keyboard.d:
         s.x += 3
-    for i in range(len(bully)):
-        for j in range(len(setupp)):
-            if bully[i].colliderect(setupp[j]):
-                jb += 1
-                if leng == 0:
-                    for i in range(lvl):
-                        setupp[0:-1].pos = gap * (i+1), 20
-                    lvl += 1
-                    score += 1
-                    jb = 0  
-                    setup() 
-    for i in range(lvl):
-        if setupp[i].colliderect(s):
+    for b in bully:
+        for r in setupp:
+            if b.colliderect(r):
+                bully.remove(b)
+                setupp.remove(r)  
+                break
+    if not setupp:
+        lvl += 1
+        setup()  
+    for r in setupp:
+        if r.colliderect(s):
             exit()
+    if lvl > 10:
+        setupp = []
+        bully = []
     pass
 
 def on_key_down():
     if keyboard.space:
+        m.play()
         b = Actor("bullet")
         bully.append(b)
         b.pos = s.x, s.y
