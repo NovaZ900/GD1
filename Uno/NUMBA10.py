7d999f3a917b8da6ca97a8a059ab5322b6457b4d
NovaZ900 <ediaebosede@gmail.com>
2025-11-20 19:48:55 +0000

    ghdhdddddddddhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ Uno/NUMBA10.py
@@ -0,0 +1,50 @@
+import pgzrun
+import random
+
+WIDTH = 350
+HEIGHT = 350
+
+sercll = []
+u = False
+
+def on_mouse_move(pos):
+    if u == True:
+        sercll.append(pos)
+
+a = 6
+r = 0
+g = 0
+b = 0
+
+def draw():
+    global r, g, b
+    screen.fill("white")
+    for pos in sercll:
+        screen.draw.filled_circle((pos), a, (r,g,b))
+
+
+def update():
+    global a, r, g, b, sercll, u
+    if keyboard.i:
+        a -= 1
+    if keyboard.p:
+        a += 1
+    if keyboard.c:
+        r = random.randint(0,255)
+        g = random.randint(0,255)
+        b = random.randint(0,255)
+    if keyboard.x:
+        r = 0
+        g = 0
+        b = 0
+    if keyboard.l:
+        sercll = []
+    if keyboard.k:
+        if u == True:
+            u = False
+    if keyboard.j:
+        if u == False:
+            u = True
+    pass
+
+pgzrun.go()
