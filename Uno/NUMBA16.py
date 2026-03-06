21931dde40d11553d2ee80b3ed4010779bb484ca
NovaZ900 <ediaebosede@gmail.com>
2026-03-06 20:03:20 +0000

    jdjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM

--- Uno/NUMBA16.py
+++ Uno/NUMBA16.py
@@ -1,7 +1,9 @@
 import pygame
 
 pygame.init()
-bg = pygame.display.set_mode((700,700))
+HEIGHT = 700
+WIDTH = 700
+bg = pygame.display.set_mode((WIDTH,HEIGHT))
 ih = pygame.image.load("Uno\images\VOIDbg.png")
 
 bg.blit(ih,(0,0))
@@ -13,11 +15,33 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("Uno\images\Tanko.png")
         self.image = pygame.transform.scale(self.image,(115,70))
-        self.rect = self.image.get_rect(14,23)
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            break
-    pygame.display.update()
+        self.rect = self.image.get_rect()
+    def update(self,pressed_keys):
+        if pressed_keys[pygame.K_a]:
+            self.rect.move_ip(-5,0)
+        if pressed_keys[pygame.K_d]:
+            self.rect.move_ip(5,0)
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > 700:
+            self.rect.right = 700
+        if self.rect.bottom >= HEIGHT:
+            self.rect.bottom = HEIGHT
+        elif self.rect.top <= 0:
+            self.rect.top = 0
+sprite = pygame.sprite.Group()
+def start():
+    aih = Player()
+    sprite.add(aih)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                break
+        pressed_keys = pygame.key.get_pressed()
+        aih.update(pressed_keys)
+        bg.blit(pygame.image.load("Uno\images\VOIDbg.png"),(0,0))
+        sprite.draw(bg)
+        pygame.display.update()
+start()
